ad0d97a35a29f47f752f4932ece738df291a03fd
Trammell Hudson <hudson@lowerlayer.nl>
2021-05-19 12:46:38 +0000

    init: add config.sh and use dhcp to configure ethernet

--- initramfs/response/startup.sh
+++ initramfs/response/startup.sh
@@ -4,10 +4,3 @@ echo "hello world!"
 DEV="/dev/sda"
 URL="http://10.0.2.2:8000/focal-server-cloudimg-amd64.img"
 URL_HASH="b723a8fa028d6ab80242239c52c0ef7d11d9dc534b491094fbc44e8d908a76ee"
-
-# force a reboot on failure
-die() {
-	echo >&2 "$@"
-	sleep 10
-	reboot
-}
