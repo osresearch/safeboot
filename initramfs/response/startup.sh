09e0b14cccc2bf8d1faeda777d66c8098b3936ec
Trammell Hudson <hudson@trmm.net>
2021-06-09 14:41:01 +0000

    bootscript: use sha256sum formatted hash file

--- initramfs/response/startup.sh
+++ initramfs/response/startup.sh
@@ -2,5 +2,4 @@
 echo "hello world!"
 
 DEV="/dev/sda"
-URL="http://10.0.2.2:8000/focal-server-cloudimg-amd64.img"
-URL_HASH="b723a8fa028d6ab80242239c52c0ef7d11d9dc534b491094fbc44e8d908a76ee"
+URL="http://10.0.2.2:8080/focal-server-cloudimg-amd64.img"
