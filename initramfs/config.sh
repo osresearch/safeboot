fac5aaa1a748d37791b96a87fa00051df5d74d00
Trammell Hudson <hudson@trmm.net>
2021-06-10 15:50:39 +0000

    attest-server: move enrollment out of Makefile, use a per-device directory in build/attest instead of sqlite

--- initramfs/config.sh
+++ initramfs/config.sh
@@ -2,3 +2,5 @@
 export ETH_DEV="eth0"
 export ATTEST_SERVER="10.0.2.2"
 export ATTEST_URL="http://$ATTEST_SERVER:8080/attest"
+export ROOTFS_URL="http://$ATTEST_SERVER:8080/focal-server-cloudimg-amd64.img"
+export ROOTFS_DEV="/dev/sda"
